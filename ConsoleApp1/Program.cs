a3d47b5b0494da841d4b3bbff729ba205fd06375
Ioana <ioana.sabau@gmail.com>
2022-03-09 22:31:34 +0200

    1st change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,65 +10,8 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            /*
-            int a = 21;
-            int c;
-            c = a;
-            Console.WriteLine("Line 1 - =  Value of c = {0}", c);
 
-            c += a;
-            Console.WriteLine("Line 2 - += Value of c = {0}", c);
-
-            c -= a;
-            Console.WriteLine("Line 3 - -=  Value of c = {0}", c);
-
-            c *= a;
-            Console.WriteLine("Line 4 - *=  Value of c = {0}", c);
-
-            c /= a;
-            Console.WriteLine("Line 5 - /=  Value of c = {0}", c);
-
-            c = 200;
-            c %= a;
-            Console.WriteLine("Line 6 - %=  Value of c = {0}", c);
-
-            c <<= 3;
-            Console.WriteLine("Line 7 - <<=  Value of c = {0}", c);
-
-            c >>= 3;
-            Console.WriteLine("Line 8 - >>=  Value of c = {0}", c);
-
-            c &= 2;
-            Console.WriteLine("Line 9 - &=  Value of c = {0}", c);
-
-            c ^= 2;
-            Console.WriteLine("Line 10 - ^=  Value of c = {0}", c);
-
-            c |= 2;
-            Console.WriteLine("Line 11 - |=  Value of c = {0}", c);
-            Console.ReadLine();
-            */
-
-            /*Book book1 = new Book();
-            book1.title = "dasfa";
-            book1.author = "dsafa";
-            book1.pages = 200;*/
-
-            var dates = new[]
-            {
-                DateTime.UtcNow.AddHours(-1),
-                DateTime.UtcNow,
-                DateTime.UtcNow.AddHours(+1),
-
-            };
-
-            foreach (var anonymous in
-             dates.Select(
-                 date => new { Formatted = $"{date:MMM dd, yyyy hh:mm zzz}", date.Ticks }))
-
-            { Console.WriteLine($"Ticks: {anonymous.Ticks}, formatted: {anonymous.Formatted}"); }
-
-
+            Console.WriteLine("Acesta e un test");
 
         }
     }
