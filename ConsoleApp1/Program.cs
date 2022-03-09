a7cc93ee02f96eb75ecee712f1b399754d306443
Ioana <ioana.sabau@gmail.com>
2022-03-09 22:33:22 +0200

    change user1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Acesta e un test");
+            Console.WriteLine("Acesta e un test, modifica userul1");
 
         }
     }
